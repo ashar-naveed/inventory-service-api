9511745eb03f34e5179e3475b489285ad8899126
Muhammad Ashar Naveed <manaveed.pro@gmail.com>
2026-04-18 07:13:55 +0300

    Prepare for deployment (env + procfile)

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:professional_use@localhost:5432/inventory_db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
