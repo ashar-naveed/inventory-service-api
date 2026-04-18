149c2a5f53384337dc5555b81363a67bb5e5eb2f
Muhammad Ashar Naveed <manaveed.pro@gmail.com>
2026-04-18 07:22:58 +0300

    Add psycopg2 for production DB

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Fix for Railway
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
